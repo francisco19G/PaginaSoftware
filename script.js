3f539080979429e0ac00ee0598969bccbf152734
Francisco <paco.delacruz56@gmail.com>
2026-03-21 19:58:53 -0600

    prueba

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql2');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// Esta es la configuración (Cadena de conexión)
-const connection = mysql.createConnection({
-  host: 'mysql://root:********@caboose.proxy.rlwy.net:52132/railway',
-  user: 'tu_usuario',
-  password: 'tu_password',
-  database: 'nombre_de_tu_bd'
+// Servir archivos estáticos (CSS, Imágenes, JS del navegador)
+app.use(express.static(path.join(__dirname, '/')));
+
+// Ruta para mostrar tu página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error conectando a la base de datos: ' + err.stack);
-    return;
-  }
-  console.log('Conectado con éxito a la base de datos.');
+// Usar el puerto que Render asigna automáticamente
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
 });
-
-module.exports = connection;
